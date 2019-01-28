c14cde75e9fd9e8ed9422c3b0fe506141cb12bb9
Shyhiem Gilbert <shyhiemg@icloud.com>
2019-01-28 18:41:40 +0000

    Set javascript to listen for button clicks

---
+++ Calculator/Calculator/Scripts/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+﻿//$(#num7) --  selecting an element
+// $(".numbers") --  refers as add for multiple elements
+
+//so it takes whole document to search for keypad instead of just above
+//when compiled.
+
+$(document).ready(function () {
+
+    //whenever something is clicked, this function needs to be performed
+    $(".keypad").click(function () {
+
+        alert("hello");
+    });
+});
+
+
