1b9785ac93164905948d8da4cfb169f1a470c561
Shyhiem Gilbert <shyhiemg@icloud.com>
2019-01-30 14:08:43 +0000

    Trying to get javascript to work for calculator

--- Calculator/Calculator/Scripts/Calculator/Calculator.js
+++ Calculator/Calculator/Scripts/Calculator/Calculator.js
@@ -4,6 +4,8 @@
 //so it takes whole document to search for keypad instead of just above
 //when compiled.
 
+
+var globalVariable = ""; //This is how you define a global not declard inside any function and hence retains its value
 $(document).ready(function () {
 
     //e is the element
@@ -25,9 +27,39 @@ $(document).ready(function () {
 
             result = result + value;
             $("#result").val(result);
-        }
-        else
-        {
+
+            //logic to populate the new textboxes
+            var operators = ["+", "-", "*", "/"]; 
+            var oldOp = $("#oldOperator").val();    //These are not global variables these will be reinitialised on every click
+            var newOp = $("#newOperator").val();    //These are not global variables these will be reinitialised on every click
+            var op1 = $("#op1").val();              //These are not global variables these will be reinitialised on every click
+            var op2 = $("#op2").val();              //These are not global variables these will be reinitialised on every click
+            if ($.inArray(value.operators) != -1) {
+                
+                if ($.trim(oldOp) == "") {
+                    $("#oldOperator").val(value);
+                } else {
+                    if ($.trim(newOp) == "") {
+                        $("#newOperator").val(value);
+
+                };
+            } else {
+                //button pressed is a number
+                
+
+                if (oldOp == undefined || $.trim(oldOp) == "") {
+                    //this is op1
+                    //We will concatenate this number to op1 and
+                    //not replace since we can have multiple digits
+                    $("#op1").val(op1 + value);
+                } else {
+                    //This is op2
+                    //We will concatenate this number to op2 and
+                    //not replace since we can have multiple digits
+                    $("#op2").val(op2 + value);
+                }
+            }
+        } else {
             return false;
         }
     });
